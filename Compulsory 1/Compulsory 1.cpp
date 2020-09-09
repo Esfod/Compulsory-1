119ef43ae6f9cbabef7f4b39c16fdd9eebf3c829
Esfod <esfod2001@gmail.com>
2020-09-09 11:12:14 +0200

    Adding a method so the player can't use the same number

--- Compulsory 1/Compulsory 1.cpp
+++ Compulsory 1/Compulsory 1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
+#include <vector>;
 
 char numbs[9]{ '1','2','3','4','5','6','7','8','9' };
 const char playerSymbol[2]{ 'X', 'O' };
-int win = 0; // if win = 1, player 1 won. if value 2 player 2 won
-//ikke mulig å velge samme tall
+std::vector<int> logg;
 
 class call
 {
 public:
-	void print() //Does all the printiting to console
+	void print(int win) //Does all the printiting to console
 	{
 		const std::string space = "--- --- ---";
 		system("cls");
@@ -25,39 +25,40 @@ public:
 			std::cout << "Pick an number between 1 and 9.\n";
 	}
 	
-	void setIn(int a, bool player) //puts inn players symbols for the numbers
+	void setIn(int a, bool p) //puts inn players symbols for the numbers
 	{
 
 		int playerStatus;
-		if (player) playerStatus = 0;
+		if (p) playerStatus = 0;
 		else playerStatus = 1;
 		numbs[a - 1] = playerSymbol[playerStatus];
+		
 	}
 	
-	int checkWin(bool player) //checking all ways to win 
+	int checkWin(bool p, int w) //checking all ways to win 
 	{
 		if (numbs[0] == numbs[1] == numbs[2] || numbs[3] == numbs[4] == numbs[5] || numbs[6] == numbs[7] == numbs[8]) //all horizontal
 		{
-			if (player)
-				win = 1;
+			if (p)
+				w = 1;
 			else
-				win = 2;
+				w = 2;
 		}
 		else if (numbs[0] == numbs[3] == numbs[6] || numbs[1] == numbs[4] == numbs[7] || numbs[2] == numbs[5] == numbs[8]) //all vertical
 		{
-			if (player)
-				win = 1;
+			if (p)
+				w = 1;
 			else
-				win = 2;
+				w = 2;
 		}
 		else if (numbs[0] == numbs[4] == numbs[8] || numbs[2] == numbs[4] == numbs[6]) //all "cross"
 		{
-			if (player) 
-				win = 1;
+			if (p)
+				w = 1;
 			else
-				win = 2;
+				w = 2;
 		}
-		return win;
+		return w;
 	}
 	
 	void restart(bool p) //restart the game 
@@ -75,14 +76,24 @@ int main()
 	call function;
 	int input;
 	bool player = true; //if false it's player's 2 turn
+	int win = 0;// if win = 1, player 1 won. if value 2 player 2 won
 	do
 	{
-		function.print();
+		function.print(win);
 		std::cin >> input;
 		function.setIn(input, player);
-		if (player)player = false;
-		else if (!player) player = true;	
+		logg.push_back(input);
+		for (int i = 0; i < logg.size(); i++)
+		{
+			if (logg[i] == input) break;
+			else
+			{
+				if (player)player = false; // player != player
+				else if (!player) player = true;
+			}	
+		}
+		
 	} 
-	while (function.checkWin(!player) == 0);
-	function.checkWin(!player);
+	while (function.checkWin(!player, win) == 0);
+	function.checkWin(!player, win);
 }
